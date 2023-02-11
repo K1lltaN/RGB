4bca1c40cda99a036f5a16b88a1bad6f383ab0d3
Максим Синявський <killtan@Air-KilltaN.fritz.box>
2023-02-11 16:25:31 +0100

    ROFL

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('R').onmousemove = function () {
+    document.body.style.backgroundColor = "red";
+}
+
+document.getElementById('R').onmouseleave = function () {
+    document.body.style.backgroundColor = "black";
+}
+document.getElementById('G').onmousemove = function () {
+    document.body.style.backgroundColor = "green"
+}
+
+document.getElementById('G').onmouseleave = function () {
+    document.body.style.backgroundColor = "black";
+}
+document.getElementById('B').onmousemove = function () {
+    document.body.style.backgroundColor = "blue";
+}
+
+document.getElementById('B').onmouseleave = function () {
+    document.body.style.backgroundColor = "black";
+}
